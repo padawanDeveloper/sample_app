d6c5366bcdd845ca8d373db6c3a8a2920133015a
Esteban Ortiz <ematiasortiz@gmail.com>
2018-04-08 23:51:00 -0300

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  
   def home
   end
 
@@ -6,5 +7,8 @@ class StaticPagesController < ApplicationController
   end
 
   def about
+  end
+
+  def contact
   end
 end
